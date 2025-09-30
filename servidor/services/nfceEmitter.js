f0783b61f6afa44a936bcc37e4b9c9d011d89764
ClaudioLobo011 <ch.lobovccv@gmail.com>
2025-09-30 11:09:30 -0300

    Fix xml-crypto reference signature options

--- servidor/services/nfceEmitter.js
+++ servidor/services/nfceEmitter.js
@@ -1204,14 +1204,14 @@ const emitPdvSaleFiscal = async ({ sale, pdv, store, emissionDate, environment, 
     getKeyInfo: () => `<X509Data><X509Certificate>${certB64}</X509Certificate></X509Data>`,
   };
   const refXPath = "/*[local-name()='NFe']/*[local-name()='infNFe']";
-  signer.addReference(
-    refXPath,
-    [
+  signer.addReference({
+    xpath: refXPath,
+    transforms: [
       'http://www.w3.org/2000/09/xmldsig#enveloped-signature',
       'http://www.w3.org/TR/2001/REC-xml-c14n-20010315',
     ],
-    'http://www.w3.org/2000/09/xmldsig#sha1'
-  );
+    digestAlgorithm: 'http://www.w3.org/2000/09/xmldsig#sha1',
+  });
   console.debug('XPath ref:', refXPath);
   signer.computeSignature(xmlForSignature, {
     prefix: '',
