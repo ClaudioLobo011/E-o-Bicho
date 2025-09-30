d76dced9f352abe3541e47702c150a2c944f8cae
ClaudioLobo011 <ch.lobovccv@gmail.com>
2025-09-30 11:04:22 -0300

    Configure SHA-1 digest algorithm for NFC-e signature

--- servidor/services/nfceEmitter.js
+++ servidor/services/nfceEmitter.js
@@ -1198,6 +1198,7 @@ const emitPdvSaleFiscal = async ({ sale, pdv, store, emissionDate, environment, 
     idAttribute: 'Id',
     canonicalizationAlgorithm: 'http://www.w3.org/TR/2001/REC-xml-c14n-20010315',
     signatureAlgorithm: 'http://www.w3.org/2000/09/xmldsig#rsa-sha1',
+    digestAlgorithm: 'http://www.w3.org/2000/09/xmldsig#sha1',
   });
   signer.keyInfoProvider = {
     getKeyInfo: () => `<X509Data><X509Certificate>${certB64}</X509Certificate></X509Data>`,
