c977bced21c218ecb39d02f7d3728e8de68ff5e9
ClaudioLobo011 <ch.lobovccv@gmail.com>
2025-10-04 05:03:29 -0300

    fix: remove nested routers

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'sonner';
 
 import App from './app/App';
@@ -23,9 +22,7 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
       <ReactQueryDevtools initialIsOpen={false} />
       <Toaster richColors position="top-right" duration={4000} />
     </QueryClientProvider>
